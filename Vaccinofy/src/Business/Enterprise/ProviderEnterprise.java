f52159f875187e38528a1107e4fffe2ab2430fee
NencyP <panchani.n@northeastern.edu>
2022-12-08 22:20:46 -0500

    Added Enterprise Hospital Inventory and NationalDistributor Classes

---
+++ Vaccinofy/src/Business/Enterprise/ProviderEnterprise.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ADMIN
+ */
+
+public class ProviderEnterprise extends Enterprise{
+    
+    private HospitalDirectory hospitalDirectory;
+    private PharmacyDirectory pharmacyDirectory;
+    
+    public ProviderEnterprise(String name){
+        super(name, EnterpriseType.Provider);
+        hospitalDirectory = new HospitalDirectory();
+        pharmacyDirectory = new PharmacyDirectory();
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new ProviderAdminRole());
+        return roles;
+    }
+
+    public HospitalDirectory getHospitalDirectory() {
+        return hospitalDirectory;
+    }
+
+    public PharmacyDirectory getPharmacyDirectory() {
+        return pharmacyDirectory;
+    }
+    
+}
+    
